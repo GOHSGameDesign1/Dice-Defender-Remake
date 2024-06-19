e22ec140d36dbe3dac02486b0ac259309e60cc91
GOHSGameDesign1 <GOHSGameDesign1@gmail.com>
2024-06-18 17:44:36 -0700

    Added Health Enemy Functionality

---
+++ Dice Defender Remake/Assets/Scripts/HealthEnemy.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class HealthEnemy : MonoBehaviour
+{
+
+    private TextMeshPro m_TextMeshPro;
+
+    [SerializeField] private int maxHealth;
+    private int health;
+
+    private void Awake()
+    {
+        m_TextMeshPro = transform.GetChild(0).GetComponent<TextMeshPro>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = maxHealth;
+        m_TextMeshPro.text = health.ToString();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!collision.CompareTag("Projectile")) return;
+
+        if(collision.TryGetComponent(out DieNumber die))
+        {
+            TakeDamage(die.getDieNumber());
+
+            if(health <= 0)
+            {
+                Die();
+            }
+        }
+
+        if (collision.TryGetComponent(out ProjectileDeath projectileDeath))
+        {
+            projectileDeath.Die();
+        }
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+
+    public void TakeDamage(int dmg)
+    {
+        health -= dmg;
+        health = Mathf.Clamp(health, 0, maxHealth);
+        m_TextMeshPro.text = health.ToString();
+    }
+}
